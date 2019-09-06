aeb62cacaeb8a1515ffa2cd78bc8c1cf94d1cace
Kyriazo <gianniskyriazo@gmail.com>
2019-09-06 14:41:44 +0300

    Customizes header a bit.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,11 +8,11 @@ const header = () => {
             <h1 className={classes.logo}>Type-Smash.com</h1>
             <h2 className={classes.heading}>Test your typing skills!</h2>
             <nav><ul>
-                <li>Homepage</li>
-                <li>Stats</li>
-                <li>About</li>
-                <li>FAQ</li>
-                <li>Contact</li>
+                <li><a href='/'>Homepage</a></li>
+                <li><a>Stats</a></li>
+                <li><a>About</a></li>
+                <li><a>FAQ</a></li>
+                <li><a>Contact</a></li>
             </ul></nav>
         </header>
     );
