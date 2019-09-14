bf75571e0a7a83cc4d050d34d2dcbda4c3c00c57
Kyriazo <gianniskyriazo@gmail.com>
2019-09-14 18:38:33 +0300

    Reworks typearea to use css modules.

--- src/components/Typearea/Typearea.js
+++ src/components/Typearea/Typearea.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Typearea.module.css';
 
 const typearea = (props)=> {
     return (
-        <input id='typearea' onChange={props.clicked} className='typedWords' type='text'  value={props.value} />
+            <input id='typearea' onChange={props.clicked} className={classes.typedWords} type='text'  value={props.value} />
     )
 }
 
